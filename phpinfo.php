5e86da3cb618854eceb71f6b7f0cca10eeff4968
Riki Watanabe <rikiwatanabe@CentoseiSig.local>
2022-10-17 10:36:12 +0900

    first

---
+++ phpinfo.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>web page</title>
+</head>
+
+<body>
+  <h1>PHP Test</h1>
+  <h2><?php echo date('jS \o\f F Y'); ?></h2>
+  <p>
+    <?php
+    $target = mktime(10, 30, 0, 10, 15, 2022);
+    $today = time();
+    $difference = $target - $today;
+    $minutes = (int) ($difference / 60);
+    if ($minutes > 0) {
+      echo "you have {$minutes} minutes left!";
+    } else {
+      echo "the countdown is over!!!!";
+    }
+    ?>
+  </p>
+</body>
+
+</html>
