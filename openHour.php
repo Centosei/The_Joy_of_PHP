75d2b41f7e13250f752001248426229649bfafac
Riki Watanabe <rikiwatanabe@CentoseiSig.local>
2022-10-17 13:18:23 +0900

    changed control structure from if to switch

--- openHour.php
+++ openHour.php
@@ -17,12 +17,24 @@
     <h1>Welcome to the Homepage of Riki's Book Store!!!</h1>
     <?php
     date_default_timezone_set("Asia/Tokyo");
-    if (date('l') == 'Monday') {
-        echo 'sorry, we are closed today.';
-    } elseif ((date('l') == 'Saturday') | (date('i') == 'Sunday')) {
-        echo "We are open today from 9 AM to 9PM";
-    } else {
-        echo "We are open today from 10 AM to 6 PM";
+    // 
+    $today = date('l');
+    // 
+    $today = "Tuesday";
+    // 
+    switch ($today) {
+        case "Monday":
+            echo "We are closed today";
+            break;
+        case "Saturday":
+            echo "We are open today from 9 AM to 9 PM";
+            break;
+        case "Sunday":
+            echo "We are open today from 9 AM to 8 PM";
+            break;
+        case $today:
+            echo "We are open today from 10 AM to 7 PM";
+            break;
     }
     ?>
 </body>
