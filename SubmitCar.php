ddcb10ced07725a8cd13db4328547e4d67665a3b
Centosei <centoseisigarette@gmail.com>
2022-10-18 18:40:47 +0900

    OOP version

--- SubmitCar.php
+++ SubmitCar.php
@@ -14,7 +14,7 @@
     $Price = $_POST['Asking_Price'];
 
     // build SQL query
-    $query = "INSERT INTO `INVENTORY` 
+    $query = "INSERT INTO `INVENTORY`
         (`VIN`, `Make`, `Model`, `ASKING_PRICE`)
         VALUES (
             '$VIN',
@@ -26,6 +26,26 @@
     // print $query
     echo $query . "<br />";
 
+    // OOP
+    $mysqli = new mysqli("localhost", "root", "", "Cars");
+
+    // Check connection
+    if ($mysqli->connect_errno) {
+        echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+        exit();
+    }
+    echo "Connection established successfully";
+
+    // Perform query
+    if ($result = $mysqli->query($query)) {
+        echo "<p>You have successfully entered $Make $Model into the database.</p>";
+    } else {
+        echo "Error entering $VIN into the database: " . mysqli_error($mysqli) . "<br/>";
+    }
+
+    $mysqli->close();
+
+    /* Procedural
     // make connection
     $server_name = "localhost";
     $user_name = "root";
@@ -47,6 +67,7 @@
 
     // close the connection
     mysqli_close($connection);
+    */
     ?>
 </body>
 
